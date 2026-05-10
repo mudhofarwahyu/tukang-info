8f3c1aeffefb04b56370c37cb301e1f3c05daa9c
mudhofarwahyu <wahyumudhofar10@gmail.com>
2026-05-10 13:46:50 +0700

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+
+// Firebase Configuration
+var firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference to the "tukang" collection in Firebase
+var db = firebase.firestore();
